46a25ebc85f9b51c343f550706a9fe760d3e075b
Miningsleuth <166779773+Miningsleuth@users.noreply.github.com>
2026-08-15 11:28:34 +1200

    minigame code improvement

--- warioware-game/scripts/minigame_2.gd
+++ warioware-game/scripts/minigame_2.gd
@@ -12,10 +12,10 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if buttons_pressed == 8:
-		#if Global.minigames_done > 3:
-		#	get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
-		#else:
-		get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
+		if Global.minigames_done > 3:
+			get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
+		else:
+			get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
 	
 	if timer_end:
 		Global.lives -= 1
