e6bd97a69959e53351d090efb62454fff811c316
Miningsleuth <166779773+Miningsleuth@users.noreply.github.com>
2026-08-15 11:03:04 +1200

    clicker minigame

--- warioware-game/scripts/minigame_2.gd
+++ warioware-game/scripts/minigame_2.gd
@@ -11,13 +11,18 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if buttons_pressed == 4:
-		if Global.minigames_done > 3:
-			get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
-		else:
-			get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+	if buttons_pressed == 8:
+		#if Global.minigames_done > 3:
+		#	get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
+		#else:
+		get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
 	
 	if timer_end:
 		Global.lives -= 1
 		Global.minigames_done -=1
 		get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+
+
+
+func _on_button_7_pressed() -> void:
+	pass # Replace with function body.
